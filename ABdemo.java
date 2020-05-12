3b14616d2c37d55ed05de6c7551391d11bd1ec95
PratikMohite87 <mohitepratik48@gmail.com>
2020-05-12 20:11:11 +0530

    this is 1st code

---
+++ ABdemo.java
@@ -0,0 +1,28 @@
+class ABdemo{
+	public static void main(String args[]){
+		
+		Iphone obj = new Iphone();
+		SamsungS4 obj1 = new SamsungS4();
+		show(obj);
+	}
+	
+	public static void show(Phone obj){
+		obj.showConfig();
+	}
+}
+
+abstract class Phone{
+	public abstract void showConfig();
+}
+
+class Iphone extends Phone{
+	public void showConfig(){
+		System.out.println("Iphone");
+	}
+}
+
+class SamsungS4 extends Phone{
+	public void showConfig(){
+		System.out.println("SamsungS4");
+	}
+}
